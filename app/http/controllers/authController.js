c9732715cae0cc881e75ce4801e47d1f5b9266ad
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2022-09-20 10:47:50 +0430

    coding login

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,7 +1,7 @@
 const { validationResult, Result } = require("express-validator");
 const userModel = require("../../models/user");
 const { expressvalidatorMapper } = require("../../modules/expressValidator");
-const { hashString } = require("../../modules/hashString");
+const { hashString, compareDataWithHashedPassword } = require("../../modules/hashString");
 
 class authController {
   //register = signUp
@@ -16,11 +16,11 @@ class authController {
           mobile,
           password: hashedPassword,
         })
-        .catch((err) => {
-          if (err?.code == 11000) {
-            throw {status :  400 , success : false , message : "the username is duplicate"};
-          }
-        });
+        // .catch((err) => {
+        //   if (err?.code == 11000) {
+        //     throw {status :  400 , success : false , message : "the username is duplicate"};
+        //   }
+        // });
       // return res.status(200).json(user);
       let result = validationResult(req);
       if (result?.errors?.length < 1) {
@@ -35,7 +35,23 @@ class authController {
       next(error);
     }
   }
-  signIn() {}
+ async signIn(req,res,next) {
+try {
+  const {username , password} = req.body ; 
+  const user = await userModel.findOne({username});
+  if(!user) throw {status : 401 , success : false , message : " the username or password is incorrect" };
+  if(!compareDataWithHashedPassword(password ,user.password)) throw {status : 401 , success : false , message : " the username or password is incorrect" };
+  return res.status(200).json({
+    status : 200 , 
+    success : true , 
+    message : " you are successfully login to your accoant",
+    token : ""
+  })
+} catch (error) {
+  next(error)
+}
+
+ }
   resetPassword() {}
 }
 
--- app/router/aouthRouter.js
+++ app/router/aouthRouter.js
@@ -1,10 +1,12 @@
 const {authController} = require("../http/controllers/authController");
 const { expressvalidatorMapper } = require("../http/middleWare/checkErrors");
-const { signUpValidator } = require("../http/validations/authValidation");
+const { signUpValidator, signInValidator } = require("../http/validations/authValidation");
 const router = require("express").Router();
 
 
-router.post("/sign-up" , signUpValidator() ,expressvalidatorMapper , authController.signUp)
+router.post("/sign-up" , signUpValidator() ,expressvalidatorMapper , authController.signUp);
+
+router.post("/sign-in" , signInValidator() ,expressvalidatorMapper , authController.signIn);
 
 module.exports = {
     authRoter : router
